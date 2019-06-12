8f9cf756538ece1fb77912cffe45ce8687630a22
Wahyu Nugroho Indrawinata <korodarmo@gmail.com>
2019-06-12 22:55:03 +0700

    a

--- CV/send.php
+++ CV/send.php
@@ -1,77 +1,90 @@
 <?php
+
+require 'PHPMailer/PHPMailerAutoload.php';
+$mail = new PHPMailer;
+$user = 'noreplyhrptlintasmediatama@gmail.com'; 
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-$error = "";
+$error = true;
 $errorMessage = 'Sorry your message can not be sent.';
 
 //Validate first
 if(empty($name)||empty($email)||empty($message)) 
 {
-    echo "Name and email and message are required !";
-    header('Location: index.html');
+  echo "Name and email and message are required !";
+  header('Location: index.html');
+  $error = false;
 }
 //validate against any email injection attempts
 if(IsInjected($email))
 {
-    echo "Bad email value!";
-    header('Location: index.html');
+  $error = false;
+  echo "Bad email value!";
+  header('Location: index.html');
 }
 
 
-$msg =  " Name : $name \r\n"; 
-$msg .= " Email: $email \r\n";
-$msg .= " WebSite: $website \r\n";
-$msg .= " Subject: $subject \r\n";
-$msg .= " Message : ".stripslashes($_POST['message'])."\r\n\n";
-$msg .= "User information \r\n"; 
-$msg .= "User IP : ".$_SERVER["REMOTE_ADDR"]."\r\n"; 
-$msg .= "Browser info : ".$_SERVER["HTTP_USER_AGENT"]."\r\n"; 
-$msg .= "User come from : ".$_SERVER["SERVER_NAME"]."\r\n";
+$mail->isSMTP();
+$mail->Host = 'ssl://smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = $user;
+$mail->Password = 'superuser';
+$mail->SMTPSecure = 'tls';
+$mail->Port = 465;
+$mail->setFrom($user, 'NATANAWA.WEB.ID');
+
+$msg =  " Name : $name <br>"; 
+$msg .= " Email: $email <br>";
+$msg .= " WebSite: $website <br>";
+$msg .= " Subject: $subject <br>";
+$msg .= " Message : ".stripslashes($_POST['message'])."<br>\n";
+$msg .= "User information <br>"; 
+$msg .= "User IP : ".$_SERVER["REMOTE_ADDR"]."<br>"; 
+$msg .= "Browser info : ".$_SERVER["HTTP_USER_AGENT"]."<br>"; 
+$msg .= "User come from : ".$_SERVER["SERVER_NAME"]."<br>";
 $msg .= "Template Name : SPLIT VCARD";
 
-$recipient = "korodarmo@gmail.com";// Change the recipient email adress to your adrees  
 $sujet =  "Sender information";
-$mailheaders = "From: $email\r\nReply-To: $email\r\nReturn-Path: $email\r\n";
 
-if (!$error){
+if ($error){
+  $mail->addAddress('korodarmo@gmail.com');
+  $mail->Subject = $sujet;
+  $mail->isHTML(true);
+  $mail->Body = $msg;
 
-		$sending = mail($recipient, $sujet, $msg, $mailheaders); 
-		
-		if ($sending) {
-				// If the message is sent we output a string to use it 
-				echo "SENDING"; 
-			} else {
-				// Display Error Message
-				echo $errorMessage; 
-			}
-	} else {
-		echo $error; // Display Error Message
-	}
+  if($mail->send()){
+    echo "SENDING"; 
+  } else {
+    echo $errorMessage; 
+  }
+} else {
+  echo $errorMessage; 
+}
 
 
-// Function to validate against any email injection attempts
 function IsInjected($str)
 {
   $injections = array('(\n+)',
-              '(\r+)',
-              '(\t+)',
-              '(%0A+)',
-              '(%0D+)',
-              '(%08+)',
-              '(%09+)'
-              );
+    '(\r+)',
+    '(\t+)',
+    '(%0A+)',
+    '(%0D+)',
+    '(%08+)',
+    '(%09+)'
+  );
   $inject = join('|', $injections);
   $inject = "/$inject/i";
   if(preg_match($inject,$str))
-    {
+  {
     return true;
   }
   else
-    {
+  {
     return false;
   }
 }
-   
+
 ?>
